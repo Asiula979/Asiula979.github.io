083b0630fafeb83167672621c7340a612322a66b
Asiula979 <janochajoanna4@gmail.com>
2021-04-02 12:25:22 +0200

    Lekcja9

--- Lekcja 9/script.js
+++ Lekcja 9/script.js
@@ -1,13 +1,15 @@
-const views=[{
-    "title": "Miasto o zachodzie słońca";
-    "src": "1.jpg";
+const views=[
+    {
+    "title": "Miasto o zachodzie słońca",
+    "src": "1.jpg",
 }
 {
-    "title": "Widok miasta z góry";
-    "src": "2.jpg";
+    "title": "Widok miasta z góry",
+    "src": "2.jpg",
 }
 {
-    "title": "Wieżowce";
-    "src": "3.jpg"; 
+    "title": "Wieżowce",
+    "src": "3.jpg", 
 }
 ]
+console.log(views);
