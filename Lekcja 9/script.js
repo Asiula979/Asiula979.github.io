642ed691cf5417b9dfb5d870f727afdeae4d0c48
Asiula979 <janochajoanna4@gmail.com>
2021-04-02 11:26:30 +0200

    Lekcja 9

---
+++ Lekcja 9/script.js
@@ -0,0 +1,13 @@
+const views=[{
+    "title": "Miasto o zachodzie słońca";
+    "src": "1.jpg";
+}
+{
+    "title": "Widok miasta z góry";
+    "src": "2.jpg";
+}
+{
+    "title": "Wieżowce";
+    "src": "3.jpg"; 
+}
+]
