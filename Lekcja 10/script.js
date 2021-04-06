9b5202e2c32400854157a01ef9e032cf9f44c1d5
Asiula979 <janochajoanna4@gmail.com>
2021-04-06 15:34:52 +0200

    Lekcja 10

---
+++ Lekcja 10/script.js
@@ -0,0 +1,52 @@
+const views=[
+    {
+    "src": "3.jpg",
+},
+{
+    "src": "4.jpg",
+},
+{
+    "src": "5.jpg", 
+}
+{
+    
+    "src": "6.jpg",
+}
+];
+console.log(views);
+
+function displayViews(index){
+const imgFild=document.querySelector("p");
+console.log(imgFild);
+imgFild.innerHTML="";
+
+
+const img=document.createElement("img");
+img.src="img/"+ views[index].src;
+imgFild.append(img);
+console.log(img);
+}
+displayViews(0);
+
+const leftArr=document.querySelector(".left");
+leftArr.onclick=()=>{changeSlide("left")};
+
+const rightArr=document.querySelector(".right");
+rightArr.onclick=()=>{changeSlide("right")};
+
+let currentSlide=0;
+function changeSlide(direction){
+    if(direction=="left"){
+        currentSlide--;
+        if(currentSlide<0){
+            currentSlide=views.length-1;
+        }
+    }else{
+        currentSlide++;
+        if(currentSlide>views.length-1){
+            currentSlide=0;   
+        }
+        }
+        console.log(currentSlide);
+        displayViews(currentSlide);
+    }
